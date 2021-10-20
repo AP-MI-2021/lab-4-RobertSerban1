0d27cac96f7cec8c9b300b051eaf211f2943d643
Robert Serban <serbanrobert43@yahoo.com>
2021-10-20 18:53:15 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def printMenu():
     print("1.Citire")
     print("2.Afisarea tuturor numerelor negative nenule din lista")
+    print("3.Afișarea celui mai mic număr care are ultima cifră egală cu o cifră citită de la tastatură.")
     print("x.Iesire")
 def citireLista():
     l = []
@@ -23,9 +24,25 @@ def afis_nr_negative(l):
     return l
 def test_afis_nr_negative():
     assert afis_nr_negative([-1, 2, 0, 3, -4]) == [-1, -4]
+def ultima_cif_egal_nr(l, x):
+    """
+    Afișarea celui mai mic număr care are ultima cifră egală cu o cifră citită de la tastatură.
+    :param l: lista de nr intregi
+    :param x: un numar intreg
+    :return: numarul care are ultima cifră egală cu o cifră citită
+    """
+    min = 999999
+    for i in l:
+        if i % 10 == x:
+            if i < min:
+                min = i
+    return min
+def test_ultima_cif_egal_nr():
+    assert ultima_cif_egal_nr([1, 6, 34, 68, 40, 48, 20], 8) == 48
 
 def main():
     test_afis_nr_negative()
+    test_ultima_cif_egal_nr()
     l = []
     while True:
         printMenu()
@@ -34,6 +51,9 @@ def main():
             l = citireLista()
         elif optiune == "2":
             print(test_afis_nr_negative(l))
+        elif optiune == "3":
+            x = int(input("Dati un numar"))
+            print(test_ultima_cif_egal_nr(l, x))
         elif optiune == "x":
             break
 
